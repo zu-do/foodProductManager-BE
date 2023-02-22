4e25785324535104d3466eb4502859202abd29de
rokman123 <rokismank@gmail.com>
2023-02-22 13:42:00 +0200

    Updated DB so User can have couple of Shelves

---
+++ PVP_Projektas_API/Migrations/20230222113740_AddShelvesUser.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PVP_Projektas_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddShelvesUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DbUsers_DbShelves_ShelfId",
+                table: "DbUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DbUsers_ShelfId",
+                table: "DbUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ShelfId",
+                table: "DbUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "DbShelves",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DbShelves_UserId",
+                table: "DbShelves",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DbShelves_DbUsers_UserId",
+                table: "DbShelves",
+                column: "UserId",
+                principalTable: "DbUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_DbShelves_DbUsers_UserId",
+                table: "DbShelves");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DbShelves_UserId",
+                table: "DbShelves");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "DbShelves");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ShelfId",
+                table: "DbUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DbUsers_ShelfId",
+                table: "DbUsers",
+                column: "ShelfId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_DbUsers_DbShelves_ShelfId",
+                table: "DbUsers",
+                column: "ShelfId",
+                principalTable: "DbShelves",
+                principalColumn: "Id");
+        }
+    }
+}
--- PVP_Projektas_API/Models/User.cs
+++ PVP_Projektas_API/Models/User.cs
@@ -6,5 +6,5 @@ public class User
     public int Id { get; set; }
     public string Name { get; set; } = null!;
     public string Lastname { get; set; } = null!;
-    public Shelf? Shelf { get; set; } // user may or may not have a shelf
+    public List<Shelf>? Shelves { get; set; } // user may or may not have a shelf
 }
