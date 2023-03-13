29386e979b035b63fbc8db5ace2d90facf77285a
AistisK22 <aiskai1@ktu.lt>
2023-03-13 22:37:04 +0200

    Implemented shelf controller and getAll method

---
+++ PVP_Projektas_API/Controllers/ShelfController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PVP_Projektas_API.Interfaces;
+using PVP_Projektas_API.Models;
+
+namespace PVP_Projektas_API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ShelfController : ControllerBase
+    {
+        private readonly IShelfRepository _shelfRepository;
+
+        public ShelfController(IShelfRepository shelfRepository)
+        {
+            _shelfRepository = shelfRepository;
+        }
+
+        [HttpGet("getAll")]
+        public async Task<List<Shelf>> GetAllShelves()
+        {
+            return await _shelfRepository.GetAllShelves();
+        }
+    }
+}
