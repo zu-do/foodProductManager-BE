96a983b6c4e3ea4941cea95b4935244bf5e0a80b
paulinapau <paulinapozeraite@gmail.com>
2023-03-31 12:20:51 +0300

    added funcionality to add shelf

--- PVP_Projektas_API/Repository/ShelfRepository.cs
+++ PVP_Projektas_API/Repository/ShelfRepository.cs
@@ -32,5 +32,24 @@ namespace PVP_Projektas_API.Repository
         {
             return await _dbContext.DbShelves.Include(p => p.Products).ToListAsync();
         }
+        public async Task<Shelf?> AddShelfAsync(string name, int userid)
+        {
+            if (name == null)//|| userid == default(int))
+            {
+                throw new ArgumentNullException(nameof(name));
+            }
+            var shelf = new Shelf()
+            {
+                Name = name,
+                UserId = userid,
+            };
+
+            await _dbContext.AddAsync(shelf);
+            await _dbContext.SaveChangesAsync();
+
+            return shelf;
+        }
+
+
     }
 }
